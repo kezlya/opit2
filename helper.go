6ab7a41ff8951d23b764bd18f8e1effdcad60dd9
Vitaly Kezlya <vitaly@kezlya.com>
2015-07-29 17:15:25 -0400

    Trying to calculate the field

--- helper.go
+++ helper.go
@@ -179,6 +179,13 @@ func _isRight(i, right int) bool {
 	return false
 }
 
+func _isUp(i, up int) bool {
+	if i+up < Height {
+		return true
+	}
+	return false
+}
+
 func _isLeft(i, left int) bool {
 	if i-left > 0 {
 		return true
@@ -429,10 +436,238 @@ func _fieldAfter(f [][]bool,i,r int,piece string)([][]bool,int){
 		copy(a[i], row[:])
 	}
 
-	a[1][0] = true
-	a[1][1] = true
+	switch piece {
+	case "I":
+		switch r {
+		case 0:
+			if _isRight(i, 3) && _isUp() {
+				pick := _getPick(i, 3)
+				
+				a[pick][i] = true
+				a[pick][i+1] = true
+				a[pick][i+2] = true
+				a[pick][i+3] = true
+			}
+		case 1:
+			a[pick][i] = true
+			a[pick+1][i] = true
+			a[pick+2][i] = true
+			a[pick+3][i] = true
+		}
+	case "J":
+		switch r {
+		case 0:
+			if _isRight(i, 2) {
+				pick := _getPick(i, 2)
+				a[i] = pick + 2
+				a[i+1] = pick + 1
+				a[i+2] = pick + 1
+				y = pick + 2
+			}
+		case 1:
+			if _isRight(i, 1) {
+				if c[i+1] >= c[i]+2 {
+					a[i] = c[i+1] + 1
+					a[i+1] = c[i+1] + 1
+					y = c[i+1] + 1
+				} else {
+					a[i] = c[i] + 3
+					a[i+1] = c[i] + 3
+					y = c[i] + 3
+				}
+			}
+		case 2:
+			if _isRight(i, 2) {
+				pick := _getPick(i, 2)
+				if pick == c[i+2] {
+					a[i] = pick + 2
+					a[i+1] = pick + 2
+					a[i+2] = pick + 2
+					y = pick + 2
+				} else {
+					a[i] = pick + 1
+					a[i+1] = pick + 1
+					a[i+2] = pick + 1
+					y = pick + 1
+				}
+			}
+		case 3:
+			if _isRight(i, 1) {
+				pick := _getPick(i, 1)
+				a[i] = pick + 1
+				a[i+1] = pick + 3
+				y = pick + 3
+			}
+		}
+	case "L":
+		switch r {
+		case 0:
+			if _isRight(i, 2) {
+				pick := _getPick(i, 2)
+				a[i] = pick + 1
+				a[i+1] = pick + 1
+				a[i+2] = pick + 2
+				y = pick + 2
+			}
+		case 1:
+			if _isRight(i, 1) {
+				pick := _getPick(i, 1)
+				a[i] = pick + 3
+				a[i+1] = pick + 1
+				y = pick + 3
+			}
+		case 2:
+			if _isRight(i, 2) {
+				pick := _getPick(i, 2)
+				if pick == c[i] {
+					a[i] = pick + 2
+					a[i+1] = pick + 2
+					a[i+2] = pick + 2
+					y = pick + 2
+				} else {
+					a[i] = pick + 1
+					a[i+1] = pick + 1
+					a[i+2] = pick + 1
+					y = pick + 1
+				}
+			}
+		case 3:
+			if _isRight(i, 1) {
+				if c[i] >= c[i+1]+2 {
+					a[i] = c[i] + 1
+					a[i+1] = c[i] + 1
+					y = c[i] + 1
+				} else {
+					a[i] = c[i+1] + 3
+					a[i+1] = c[i+1] + 3
+					y = c[i+1] + 3
+				}
+			}
+		}
+	case "O":
+		if _isRight(i, 1) {
+			pick := _getPick(i, 1)
+			a[i] = pick + 2
+			a[i+1] = pick + 2
+			y = pick + 2
+		}
+	case "S":
+		switch r {
+		case 0:
+			if _isRight(i, 2) {
+				pick := _getPick(i, 2)
+				if pick == c[i] || pick == c[i+1] {
+					a[i] = pick + 1
+					a[i+1] = pick + 2
+					a[i+2] = pick + 2
+					y = pick + 2
+				} else {
+					a[i] = pick
+					a[i+1] = pick + 1
+					a[i+2] = pick + 1
+					y = pick + 1
+				}
+			}
+		case 1:
+			if _isRight(i, 1) {
+				pick := _getPick(i, 1)
+				if pick == c[i+1] {
+					a[i] = pick + 3
+					a[i+1] = pick + 2
+					y = pick + 3
+				} else {
+					a[i] = pick + 2
+					a[i+1] = pick + 1
+					y = pick + 2
+				}
+			}
+		}
+	case "T":
+		switch r {
+		case 0:
+			if _isRight(i, 2) {
+				pick := _getPick(i, 2)
+				a[i] = pick + 1
+				a[i+1] = pick + 2
+				a[i+2] = pick + 1
+				y = pick + 2
+			}
+		case 1:
+			if _isRight(i, 1) {
+				pick := _getPick(i, 1)
+				if pick == c[i] {
+					a[i] = pick + 3
+					a[i+1] = pick + 2
+					y = pick + 3
+				} else {
+					a[i] = pick + 2
+					a[i+1] = pick + 1
+					y = pick + 2
+				}
+			}
+		case 2:
+			if _isRight(i, 2) {
+				pick := _getPick(i, 2)
+				if pick == c[i+1] {
+					a[i] = pick + 2
+					a[i+1] = pick + 2
+					a[i+2] = pick + 2
+					y = pick + 2
+				} else {
+					a[i] = pick + 1
+					a[i+1] = pick + 1
+					a[i+2] = pick + 1
+					y = pick + 1
+				}
+			}
+		case 3:
+			if _isRight(i, 1) {
+				pick := _getPick(i, 1)
+				if pick == c[i+1] {
+					a[i] = pick + 2
+					a[i+1] = pick + 3
+					y = pick + 3
 
-	return a	, 1
+				} else {
+					a[i] = pick + 1
+					a[i+1] = pick + 2
+					y = pick + 2
+				}
+			}
+		}
+	case "Z":
+		switch r {
+		case 0:
+			if _isRight(i, 2) {
+				pick := _getPick(i, 2)
+				if pick == c[i+1] || pick == c[i+2] {
+					a[i] = pick + 2
+					a[i+1] = pick + 2
+					a[i+2] = pick + 1
+					y = pick + 2
+				} else {
+					a[i] = pick + 1
+					a[i+1] = pick + 1
+					a[i+2] = pick
+					y = pick + 1
+				}
+			}
+		case 1:
+			if _isRight(i, 1) {
+				pick := _getPick(i, 1)
+				if pick == c[i] {
+					a[i] = pick + 2
+					a[i+1] = pick + 3
+					y = pick + 3
+				} else {
+					a[i] = pick + 1
+					a[i+1] = pick + 2
+					y = pick + 2
+				}
+			}
+		}
+	}
+	return a, y
 }
 
 func _getPick(i, v int) int {
