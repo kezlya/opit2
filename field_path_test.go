a3b35ac95dbd4cb10c60183961b0aace47a6ad00
Vitaly Kezlya <vitaly@kezlya.com>
2015-08-31 18:48:38 -0700

    finishing tests

--- field_path_test.go
+++ field_path_test.go
@@ -7,72 +7,36 @@ import (
 
 var arangePathField = Field{{true, false, true, true, true, true, true, true, true, true}, {true, true, true, true, true, true, true, true, true, true}, {true, true, true, true, true, false, true, true, true, true}, {true, true, true, true, true, true, true, true, true, true}, {true, true, true, true, true, true, true, true, true, true}, {true, true, true, true, true, true, true, false, true, true}, {true, true, false, true, true, true, true, true, true, true}, {true, true, true, true, true, true, true, true, true, true}, {true, true, true, true, true, true, true, true, true, true}, {true, true, true, true, true, true, true, true, true, true}, {false, true, true, true, false, true, true, true, true, false}, {false, true, true, false, false, false, false, true, false, false}, {false, false, true, true, false, false, false, false, false, false}, {false, false, false, true, false, false, false, true, false, false}, {false, false, true, true, false, false, true, true, true, false}, {false, false, true, true, false, false, false, false, true, false}, {false, false, true, false, false, false, false, false, true, false}, {false, false, true, true, false, false, false, false, true, false}, {false, false, true, false, false, false, false, false, false, false}, {false, false, false, false, false, false, false, false, false, false}}
 
-/*
-func Test_pathTopPos_I(t *testing.T) {
-	piece := Piece{Name: "I", Rotation: 0}
-	piece.InitSpace(Cell{X: 3, Y: 19})
-	pos1 := Position{X: 0, Rotation: 1}
-	pos2 := Position{X: 3, Rotation: 1}
-	pos3 := Position{X: 4, Rotation: 0}
-	pos4 := Position{X: 9, Rotation: 1}
-	pos5 := Position{X: 3, Rotation: 0}
-	pos6 := Position{X: 7, Rotation: 1}
-
-	path1 := arangePathField.CalculatePath(pos1, piece)
-	path2 := arangePathField.CalculatePath(pos2, piece)
-	path3 := arangePathField.CalculatePath(pos3, piece)
-	path4 := arangePathField.CalculatePath(pos4, piece)
-	path5 := arangePathField.CalculatePath(pos5, piece)
-	path6 := arangePathField.CalculatePath(pos6, piece)
-
-	if path1 != "left,left,left,turnleft,left,drop" {
-		fmt.Println("bad path1:", path1)
-		t.Fail()
-	}
-	if path2 != "turnleft,left,drop" {
-		fmt.Println("bad path2:", path2)
-		t.Fail()
-	}
-	if path3 != "right,drop" {
-		fmt.Println("bad path3:", path3)
-		t.Fail()
-	}
-	if path4 != "" {
-		fmt.Println("bad path4:", path4)
-		t.Fail()
-	}
-	if path5 != "drop" {
-		fmt.Println("bad path5:", path5)
-		t.Fail()
-	}
-	if path6 != "right,right,turnright,drop" {
-		fmt.Println("bad path6:", path6)
-		t.Fail()
-	}
-}
-*/
 func Test_path_T(t *testing.T) {
 	piece := Piece{Name: "T", Rotation: 0}
 	piece.InitSpace(Cell{X: 3, Y: 19})
 
 	hole0 := Cell{X: 3, Y: 16}
-
-	//hole1 := Cell{X: 2, Y: 13}
+	hole1 := Cell{X: 2, Y: 13}
 	hole2 := Cell{X: 3, Y: 11}
-	//hole3_bad := Cell{X: 2, Y: 6}
-	//hole4 := Cell{X: 7, Y: 12}
-	//hole5_bad := Cell{X: 8, Y: 11}
+	hole3 := Cell{X: 7, Y: 12}
+	hole_bad1 := Cell{X: 2, Y: 6}
+	hole_bad2 := Cell{X: 8, Y: 11}
 
-	//positions, total := arangePathField.FixHole(piece, hole0)
-	//arangePathField.FixHole(piece, hole1)
-	positions, total := arangePathField.FixHole(piece, []Cell{hole0, hole2})
-	//arangePathField.FixHole(piece, hole3_bad)
-	//arangePathField.FixHole(piece, hole4)
-	//arangePathField.FixHole(piece, hole5_bad)
+	good_positions, good_total := arangePathField.FixHole(piece, []Cell{hole0, hole1, hole2, hole3})
+	bad_positions, bad_total := arangePathField.FixHole(piece, []Cell{hole_bad1, hole_bad2})
 
-	for _, pos := range positions {
-		fmt.Println(pos.Moves)
+	if len(good_positions) != 12 {
+		for _, pos := range good_positions {
+			fmt.Println(pos.Moves)
+		}
+		t.Fail()
 	}
-	fmt.Println(total, "=========================", len(positions))
-	fmt.Println("done ++++++")
+
+	if len(bad_positions) != 0 {
+		for _, pos := range bad_positions {
+			fmt.Println(pos.Moves)
+		}
+		t.Fail()
+	}
+
+	if good_total != bad_total {
+		fmt.Println("should be the same", good_total, bad_total)
+		t.Fail()
+	}
 }
