8f4b5260522ff104318e7fd828d033c283e6a095
Vitaly Kezlya <vitaly@kezlya.com>
2015-09-03 22:47:23 -0700

    z turn right implemented

--- piece.go
+++ piece.go
@@ -317,60 +317,49 @@ func (p *Piece) Turnright() Piece {
 			np.CurrentX = sp["m1"].X
 			np.CurrentY = sp["m1"].Y
 		}
-		/*case "Z":
+	case "Z":
 		switch p.Rotation {
 		case 0:
-		 := Cell{X: sp[""].X , Y: sp[""].Y }
-		 := Cell{X: sp[""].X , Y: sp[""].Y }
-		delete(sp, "_")
-		delete(sp, "_")
-		sp["_"] = _
-		sp["_"] = _
-		np.CurrentX = _.X
-		np.CurrentY = _.Y
+			b2 := Cell{X: sp["t1"].X + 1, Y: sp["t1"].Y - 2}
+			t3 := Cell{X: sp["t2"].X + 1, Y: sp["t2"].Y}
+			delete(sp, "t1")
+			delete(sp, "t2")
+			sp["b2"] = b2
+			sp["t3"] = t3
+			np.CurrentX = b2.X
+			np.CurrentY = b2.Y
 		case 1:
-		 := Cell{X: sp[""].X , Y: sp[""].Y }
-		 := Cell{X: sp[""].X , Y: sp[""].Y }
-		delete(sp, "_")
-		delete(sp, "_")
-		sp["_"] = _
-		sp["_"] = _
-		np.CurrentX = _.X
-		np.CurrentY = _.Y
+			m1 := Cell{X: sp["t3"].X - 2, Y: sp["t3"].Y - 1}
+			b3 := Cell{X: sp["m3"].X, Y: sp["m3"].Y - 1}
+			delete(sp, "_")
+			delete(sp, "_")
+			sp["m1"] = m1
+			sp["b3"] = b3
+			np.CurrentX = m1.X
+			np.CurrentY = b3.Y
 		case 2:
-		 := Cell{X: sp[""].X , Y: sp[""].Y }
-		 := Cell{X: sp[""].X , Y: sp[""].Y }
-		delete(sp, "_")
-		delete(sp, "_")
-		sp["_"] = _
-		sp["_"] = _
-		np.CurrentX = _.X
-		np.CurrentY = _.Y
+			t2 := Cell{X: sp["b3"].X - 1, Y: sp["b3"].Y + 2}
+			b1 := Cell{X: sp["b2"].X - 1, Y: sp["b2"].Y}
+			delete(sp, "b3")
+			delete(sp, "b2")
+			sp["t2"] = t2
+			sp["b1"] = b1
+			np.CurrentX = b1.X
+			np.CurrentY = b1.Y
 		case 3:
-		 := Cell{X: sp[""].X , Y: sp[""].Y }
-		 := Cell{X: sp[""].X , Y: sp[""].Y }
-		delete(sp, "_")
-		delete(sp, "_")
-		sp["_"] = _
-		sp["_"] = _
-		np.CurrentX = _.X
-		np.CurrentY = _.Y
-		}*/
+			m3 := Cell{X: sp["b1"].X + 2, Y: sp["b1"].Y + 1}
+			t1 := Cell{X: sp["m1"].X, Y: sp["m1"].Y + 1}
+			delete(sp, "b1")
+			delete(sp, "b1")
+			sp["t1"] = t1
+			sp["m3"] = m3
+			np.CurrentX = t1.X
+			np.CurrentY = m3.Y
+		}
 	}
 	np.Space = sp
 	np.Key = np.key()
 	return np
-
-	/*
-		 := Cell{X: sp[""].X , Y: sp[""].Y }
-		 := Cell{X: sp[""].X , Y: sp[""].Y }
-		delete(sp, "_")
-		delete(sp, "_")
-		sp["_"] = _
-		sp["_"] = _
-		np.CurrentX = _.X
-		np.CurrentY = _.Y
-	*/
 }
 
 func (p *Piece) Turnleft() Piece {
