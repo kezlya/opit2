0d094c777692a1a05c0e85d515b1277a452bd43d
Vitaly Kezlya <vitaly@kezlya.com>
2015-10-22 19:25:55 -0400

    trying to improve result by playing with multipliers for score

--- game.go
+++ game.go
@@ -118,11 +118,11 @@ func (g *Game) asignUpdates(who, action, value string) {
 
 func (g *Game) calculateMoves() *Piece {
 	st := g.Strategy
-	if g.MyPlayer.Empty < 6 {
+	if g.MyPlayer.Empty < 5 {
 		st.Step = g.Strategy.Step + 3
 	}
-	if g.MyPlayer.Empty < 4 {
-		st.HighY = g.Strategy.HighY + (5 - g.MyPlayer.Empty)
+	if g.MyPlayer.Empty < 3 {
+		st.HighY = g.Strategy.HighY + (4 - g.MyPlayer.Empty)
 	}
 
 	trim := 0
