430b7aa54290ac8a513d0f06228fd1c419e47102
Vitaly Kezlya <vitaly@kezlya.com>
2015-12-04 12:04:26 -0800

    benchmark added for FindPositions

--- field_bench_test.go
+++ field_bench_test.go
@@ -1,45 +1,36 @@
 package main
 
-/*
 import (
 	"testing"
 )
 
-var fBench = Field{
-	Grid: [][]bool{
-		{true, true, true, false, true, true, true, true, true, false},
-		{true, true, true, true, true, true, true, true, true, true},
-		{true, true, true, true, true, false, true, true, true, false},
-		{true, true, true, true, true, true, false, true, true, true},
-		{true, true, true, true, true, false, false, true, true, true},
-		{true, false, true, true, true, true, true, true, true, true},
-		{true, true, true, true, true, true, true, true, true, true},
-		{true, true, false, true, true, true, true, true, true, true},
-		{true, true, true, true, true, false, true, true, true, true},
-		{true, true, true, true, true, true, true, true, true, true},
-		{true, false, true, true, true, false, false, false, false, true},
-		{true, false, false, false, true, false, true, false, false, true},
-		{true, false, false, true, false, true, true, false, false, false},
-		{true, false, false, true, false, false, true, false, false, false},
-		{true, false, false, true, false, false, false, false, false, false},
-		{true, false, false, false, false, false, false, false, false, false},
-		{false, false, false, false, false, false, false, false, false, false},
-		{false, false, false, false, false, false, false, false, false, false},
-		{false, false, false, false, false, false, false, false, false, false},
-		{false, false, false, false, false, false, false, false, false, false},
-	},
+var benchGrid = Grid{
+	{true, true, true, false, true, true, true, true, true, false},
+	{true, true, true, true, true, true, true, true, true, true},
+	{true, true, true, true, true, false, true, true, true, false},
+	{true, true, true, true, true, true, false, true, true, true},
+	{true, true, true, true, true, false, false, true, true, true},
+	{true, false, true, true, true, true, true, true, true, true},
+	{true, true, true, true, true, true, true, true, true, true},
+	{true, true, false, true, true, true, true, true, true, true},
+	{true, true, true, true, true, false, true, true, true, true},
+	{true, true, true, true, true, true, true, true, true, true},
+	{true, false, true, true, true, false, false, false, false, true},
+	{true, false, false, false, true, false, true, false, false, true},
+	{true, false, false, true, false, true, true, false, false, false},
+	{true, false, false, true, false, false, true, false, false, false},
+	{true, false, false, true, false, false, false, false, false, false},
+	{true, false, false, false, false, false, false, false, false, false},
+	{false, false, false, false, false, false, false, false, false, false},
+	{false, false, false, false, false, false, false, false, false, false},
+	{false, false, false, false, false, false, false, false, false, false},
+	{false, false, false, false, false, false, false, false, false, false},
 }
-*/
-/*
-func Benchmark_Picks(b *testing.B) {
-	for n := 0; n < b.N; n++ {
-		fBench.Picks()
-	}
-}
+var benchField = benchGrid.ToField()
+var benchTpiece = InitPiece("T", 3, 19)
 
-func Benchmark_FindHoles(b *testing.B) {
+func Benchmark_FindPositions(b *testing.B) {
 	for n := 0; n < b.N; n++ {
-		fBench.FindHoles()
+		benchField.FindPositions(benchTpiece)
 	}
 }
-*/
