667f1cbef838b531f849937bdf5ed8a7ba465c96
Vitaly Kezlya <vitaly@kezlya.com>
2015-12-15 17:22:43 -0800

    more constants

--- main.go
+++ main.go
@@ -7,8 +7,15 @@ import (
 	"strings"
 )
 
+// External API constants
 const I, J, L, O, S, T, Z = "I", "J", "L", "O", "S", "T", "Z"
-const down, left, right, turnleft, turnright, drop = "down", "left", "right", "turnleft", "turnright", "drop"
+const settings, update, action = "settings", "update", "action"
+const down = "down"
+const left = "left"
+const right = "right"
+const turnleft = "turnleft"
+const turnright = "turnright"
+const drop = "drop"
 
 var strategy = Strategy{
 	Burn:   2,
@@ -26,11 +33,11 @@ func main() {
 		input, _ := consolereader.ReadString('\n')
 		parts := strings.Split(strings.TrimSpace(input), " ")
 		switch parts[0] {
-		case "settings":
+		case settings:
 			game.asignSettings(parts[1], parts[2])
-		case "update":
+		case update:
 			game.asignUpdates(parts[1], parts[2], parts[3])
-		case "action":
+		case action:
 			//time, _ := strconv.Atoi(parts[2])
 			game.initPieces()
 			pos := game.calculateMoves()
