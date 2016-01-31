a2d2a8f5eef6f2872ad711348a72e147e4d2ddbf
Vitaly Kezlya <vitaly@kezlya.com>
2016-01-30 22:24:59 -0800

    fixing tests

--- sort_test.go
+++ sort_test.go
@@ -7,13 +7,13 @@ import (
 
 func Test_Sort(t *testing.T) {
 	//arrange
-	pos0 := &Piece{Score: &Score{Total: 5}}
-	pos1 := &Piece{Score: &Score{Total: 10}}
-	pos2 := &Piece{Score: &Score{Total: 3}}
-	pos3 := &Piece{Score: &Score{Total: 3}}
-	pos4 := &Piece{Score: &Score{Total: 1}}
-	pos5 := &Piece{Score: &Score{Total: 4}}
-	pos6 := &Piece{Score: &Score{Total: 2}}
+	pos0 := &Piece{Score: &Score{Total: 5}, FieldAfter: &testField}
+	pos1 := &Piece{Score: &Score{Total: 10}, FieldAfter: &testField}
+	pos2 := &Piece{Score: &Score{Total: 3}, FieldAfter: &testField}
+	pos3 := &Piece{Score: &Score{Total: 3}, FieldAfter: &testField}
+	pos4 := &Piece{Score: &Score{Total: 1}, FieldAfter: &testField}
+	pos5 := &Piece{Score: &Score{Total: 4}, FieldAfter: &testField}
+	pos6 := &Piece{Score: &Score{Total: 2}, FieldAfter: &testField}
 	pp := []*Piece{pos4, pos0, pos5, pos2, pos6, pos1, pos3}
 
 	//act
